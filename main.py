ca8e92bcbd0b8a45a99cc2cb8e2b5d7209f60aec
Maiicy <Maiicy@Foxmail.com>
2025-05-06 13:28:14 +0800

    :tada: add portal

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import streamlit
+import pandas
+import streamlit.web.cli as stcli
+import os, sys
+import app
+
+
+def resolve_path(path):
+    resolved_path = os.path.abspath(os.path.join(os.getcwd(), path))
+    return resolved_path
+
+
+if __name__ == "__main__":
+    sys.argv = [
+        "streamlit",
+        "run",
+        resolve_path("app.py"),
+        "--global.developmentMode=false",
+    ]
+    sys.exit(stcli.main())
